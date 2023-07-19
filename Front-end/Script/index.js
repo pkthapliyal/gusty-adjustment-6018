6dbea98f133c1daa0e9d87f8b49b99318fd44eb5
abhithemauryas <abhithemauryas5@gmail.com>
2023-07-19 23:50:42 +0530

    Frontend has been created and index page completed

---
+++ Front-end/Script/index.js
@@ -0,0 +1,11 @@
+window.addEventListener('scroll',function(){
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+function togglemenu(){
+    const menutoggle = document.querySelector('.menutoggle');
+    const navigation = document.querySelector('.navigation');
+    menutoggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+};
