cf124f3f62c167907960c4f886cc0a638365c241
ankita-barad <ankitabarad2111@gmail.com>
2023-07-20 20:59:00 +0530

    fw_253_day2_worked_on_frontend

--- Front-end/LawyersPage/lawyer.js
+++ Front-end/LawyersPage/lawyer.js
@@ -1,48 +1,55 @@
-// Handle form submission
-const form = document.getElementById("lawyerForm");
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+  // Slide in the form container when the lawyer lands on the page
+  const formContainer = document.querySelector(".form-container");
+  formContainer.classList.remove("slide-down");
 
-  // Perform form field validation
-  const location = document.getElementById("location").value.trim();
-  const specialization = document.getElementById("specialization").value.trim();
-  const practiceAreas = document.getElementById("practiceAreas").value.trim();
+  // Slide in the sliding box with lawyer information
+  const slidingBox = document.getElementById("lawyerInfo");
+  slidingBox.classList.add("active");
 
-  if (!location || !specialization || !practiceAreas) {
-    const errorContainer = document.createElement("p");
-    errorContainer.classList.add("form-error");
-    errorContainer.textContent = "Please fill out all required fields.";
-    form.appendChild(errorContainer);
-    return;
-  }
+  // Handle form submission
+  const form = document.getElementById("lawyerForm");
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
 
-  // Create form data
-  const formData = new FormData(form);
-  formData.set("slots", JSON.stringify(slots));
+    // Perform form field validation
+    const location = document.getElementById("location").value.trim();
+    const specialization = document
+      .getElementById("specialization")
+      .value.trim();
+    const practiceAreas = document.getElementById("practiceAreas").value.trim();
+    const image = document.getElementById("image").files[0]; // Get the uploaded image file
 
-  // Handle form submission and AJAX call here
-  // For example, you can use fetch() to post the form data to the server
+    if (!location || !specialization || !practiceAreas || !image) {
+      const errorContainer = document.createElement("p");
+      errorContainer.classList.add("form-error");
+      errorContainer.textContent = "Please fill out all required fields.";
+      form.appendChild(errorContainer);
+      return;
+    }
 
-  // Replace 'your-server-endpoint' with the actual server endpoint
-  const response = await fetch("your-server-endpoint", {
-    method: "POST",
-    body: formData,
+    // Create form data as an object
+    const formData = {
+      location: location,
+      specialization: specialization,
+      practiceAreas: practiceAreas,
+      image: image.name, // Use the file name as a placeholder
+      slots: [{ startTime: 1 }],
+    };
+    console.log(formData);
+
+    // const { location, specialization, slots, practiceAreas, image } = req.body;
+
+    // Convert the object to JSON and send it in the request body
+    const response = await fetch("http://localhost:3300/lawyer/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NGI3YWViNGI5MjRkZjE2MWJjZmZlODEiLCJpYXQiOjE2ODk4NjYxNDYsImV4cCI6MTY4OTg2OTc0Nn0.oQ2HkQv2k03DQY0EUG8PYWv4QRyLUqOwBQQUwVBWsIc`,
+      },
+      body: JSON.stringify(formData),
+    });
+    const responseData = await response.json();
+    console.log("Response:", responseData);
   });
-  const responseData = await response.json();
-  console.log("Response:", responseData);
 });
-
-// Add slot
-function addSlot() {
-  const slotsDiv = document.getElementById("slots");
-  const slotDiv = document.createElement("div");
-  slotDiv.innerHTML = `
-    <label for="day">Day:</label>
-    <input type="date" name="day" required>
-    <label for="startTime">Start Time:</label>
-    <input type="time" name="startTime" required>
-    <label for="endTime">End Time:</label>
-    <input type="time" name="endTime" required>
-  `;
-  slotsDiv.appendChild(slotDiv);
-}
