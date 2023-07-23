14c869aa9bfee57988872faed4acfb204ea53ce1
Pankaj Kumar <pkthapliyal101@gmail.com>
2023-07-23 15:03:59 +0530

    Index Login/signup final

--- Front-end/Script/index.js
+++ Front-end/Script/index.js
@@ -1,9 +1,9 @@
-window.addEventListener('scroll',function(){
+window.addEventListener('scroll', function () {
     const header = document.querySelector('header');
     header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-function togglemenu(){
+function togglemenu() {
     const menutoggle = document.querySelector('.menutoggle');
     const navigation = document.querySelector('.navigation');
     menutoggle.classList.toggle('active');
